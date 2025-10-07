6f32473ea9e6bd22279a27a7025881042f221995
Perilo <perilojp@gmail.com>
2025-10-06 21:03:13 -0300

    :bug: fix: Zod Email Schema

--- api/src/schemas/userSchema.ts
+++ api/src/schemas/userSchema.ts
@@ -2,14 +2,14 @@ import { z } from "zod";
 
 export const createUserSchema = z.object({
     nome: z.string().min(1, "Nome é obrigatório"),
-    email: z.string().email("Email inválido"),
+    email: z.email("Email inválido"),
     senha: z.string().min(6, "Senha deve ter pelo menos 6 caracteres"),
     papel: z.enum(["CLIENTE", "GERENTE"]).optional()
 });
 
 export const updateUserSchema = z.object({
     nome: z.string().min(1).optional(),
-    email: z.string().email().optional(),
+    email: z.email().optional(),
     senha: z.string().min(6).optional(),
     papel: z.enum(["CLIENTE", "GERENTE"]).optional()
 });
