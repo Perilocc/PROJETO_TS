4658407a956f82f3e0b89e91f0aa7fc66b96f54b
juliowccs <julioceasarcs@gmail.com>
2025-10-06 21:35:46 -0300

    ✨ feat: Include na categoria

--- api/src/services/categoriaService.ts
+++ api/src/services/categoriaService.ts
@@ -2,21 +2,26 @@ import prisma from "../db/prisma";
 import { Categoria } from "../generated/prisma";
 
 export const getAllCategorias = async (): Promise<Categoria[]> => {
-	return prisma.categoria.findMany();
+	return prisma.categoria.findMany({
+		include: { 
+			veiculos: true 
+		}
+	});
 }
 
 export const getCategoriaById = async (id: number): Promise<Categoria | null> => {
-	return prisma.categoria.findUnique({ where: { id } });
+	return prisma.categoria.findUnique({ where: { id }, include: { veiculos: true } });
 }
 
 export const createCategoria = async (nome: string, precoDiaria: number): Promise<Categoria> => {
 	return prisma.categoria.create({
-		data: { nome, precoDiaria }
+		data: { nome, precoDiaria },
+		include: { veiculos: true }
 	});
 }
 
 export const updateCategoria = async (id: number, data: Partial<Categoria>): Promise<Categoria> => {
-	return prisma.categoria.update({ where: { id }, data });
+	return prisma.categoria.update({ where: { id }, data, include: { veiculos: true } });
 }
 
 export const deleteCategoria = async (id: number): Promise<void> => {
